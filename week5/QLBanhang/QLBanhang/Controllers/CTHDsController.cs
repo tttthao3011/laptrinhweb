73a49d62b97ee7005992361c81abdcf1c7aff6d3
tttthao3011 <68801907+tttthao3011@users.noreply.github.com>
2020-11-25 16:15:16 +0700

    update

---
+++ week5/QLBanhang/QLBanhang/Controllers/CTHDsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLBanhang.Models;
+
+namespace QLBanhang.Controllers
+{
+    public class CTHDsController : Controller
+    {
+        private qlbanhangEntities db = new qlbanhangEntities();
+
+        // GET: CTHDs
+        public ActionResult Index()
+        {
+            var cTHDs = db.CTHDs.Include(c => c.HoaDon).Include(c => c.SanPham);
+            return View(cTHDs.ToList());
+        }
+
+        // GET: CTHDs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHD cTHD = db.CTHDs.Find(id);
+            if (cTHD == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTHD);
+        }
+
+        // GET: CTHDs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaHD = new SelectList(db.HoaDons, "MaHD", "MaKH");
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP");
+            return View();
+        }
+
+        // POST: CTHDs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaHD,MaSP,Soluong,DongiaBan,Giamgia")] CTHD cTHD)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CTHDs.Add(cTHD);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaHD = new SelectList(db.HoaDons, "MaHD", "MaKH", cTHD.MaHD);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", cTHD.MaSP);
+            return View(cTHD);
+        }
+
+        // GET: CTHDs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHD cTHD = db.CTHDs.Find(id);
+            if (cTHD == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaHD = new SelectList(db.HoaDons, "MaHD", "MaKH", cTHD.MaHD);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", cTHD.MaSP);
+            return View(cTHD);
+        }
+
+        // POST: CTHDs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaHD,MaSP,Soluong,DongiaBan,Giamgia")] CTHD cTHD)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cTHD).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaHD = new SelectList(db.HoaDons, "MaHD", "MaKH", cTHD.MaHD);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "TenSP", cTHD.MaSP);
+            return View(cTHD);
+        }
+
+        // GET: CTHDs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTHD cTHD = db.CTHDs.Find(id);
+            if (cTHD == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTHD);
+        }
+
+        // POST: CTHDs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CTHD cTHD = db.CTHDs.Find(id);
+            db.CTHDs.Remove(cTHD);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ week5/QLBanhang/QLBanhang/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using QLBanhang.Models;
+
+namespace QLBanhang.Controllers
+{
+    public class HomeController : Controller
+    {
+        qlbanhangEntities db = new qlbanhangEntities();
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.LoaiSP);
+            return View(sanPhams.ToList());
+        }
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
